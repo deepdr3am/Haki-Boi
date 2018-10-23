e86ad2e6bed2b62ce1a7e01ca33aef8b7f5c75b9
terry623 <terry92516@gmail.com>
2018-10-23 20:29:29 +0800

    add animator

---
+++ Assets/Script/AI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+
+
+	public Vector3 ballPos, spawnPos, lastPosition, dir;
+	public GameObject ball;
+	// Use this for initialization
+	void Start () {
+		ballPos = ball.transform.position;
+        spawnPos = transform.position;
+        lastPosition = spawnPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)){
+            transform.position = spawnPos;
+			ball.transform.position = ballPos;
+		}
+        dir = transform.position - lastPosition;
+        lastPosition = transform.position;
+	}
+
+	void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "ball"){
+            col.gameObject.GetComponent<Rigidbody>().AddForce(dir*5000);
+        }
+    }
+}
