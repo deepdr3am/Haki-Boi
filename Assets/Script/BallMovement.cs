894c7561c73b2f4f43b356d3969d00882938c47c
terry623 <terry92516@gmail.com>
2018-10-23 22:33:27 +0800

    add teacher

--- Assets/Script/BallMovement.cs
+++ Assets/Script/BallMovement.cs
@@ -2,31 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMovement : MonoBehaviour {
+public class BallMovement : MonoBehaviour
+{
 
-	public float min_x, max_x, min_z, max_z, forceMag;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(transform.position.x < min_x)
-		{
-			this.GetComponent<Rigidbody>().AddForce(forceMag,0.0f,0.0f);
-		}
-		else if(transform.position.x > max_x)
-		{
-			this.GetComponent<Rigidbody>().AddForce(-forceMag,0.0f,0.0f);
-		}
-		else if(transform.position.z < min_z)
-		{
-			this.GetComponent<Rigidbody>().AddForce(0.0f,0.0f,forceMag);
-		}
-		else if(transform.position.z > max_z)
-		{
-			this.GetComponent<Rigidbody>().AddForce(0.0f,0.0f,-forceMag);
-		}
-	}
+    public float min_x, max_x, min_z, max_z, forceMag;
+    public ParticleSystem effect;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < min_x)
+        {
+            this.GetComponent<Rigidbody>().AddForce(forceMag, 0.0f, 0.0f);
+        }
+        else if (transform.position.x > max_x)
+        {
+            this.GetComponent<Rigidbody>().AddForce(-forceMag, 0.0f, 0.0f);
+        }
+        else if (transform.position.z < min_z)
+        {
+            this.GetComponent<Rigidbody>().AddForce(0.0f, 0.0f, forceMag);
+        }
+        else if (transform.position.z > max_z)
+        {
+            this.GetComponent<Rigidbody>().AddForce(0.0f, 0.0f, -forceMag);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bigger")
+        {
+            transform.localScale *= 2;
+            Destroy(other.gameObject);
+
+            effect.Play();
+        }
+    }
 }
---
+++ Assets/Script/GameControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public GameObject spin;
+
+    // Use this for initialization
+    void Start()
+    {
+        spin.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            spin.active = true;
+        }
+    }
+}
